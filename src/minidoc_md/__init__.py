6baeca0ef2b1cba2432766654e6423f7a3b8669d
Milan Staffehl <milan-staffehl@gmx.de>
2026-07-26 14:21:48 +0200

    Add metadata to package `__init__.py`

--- src/minidoc_md/__init__.py
+++ src/minidoc_md/__init__.py
@@ -1,6 +1,11 @@
 from .directives import register_sphinx_version_notice_directives
 from .roles import register_sphinx_reference_roles
 
+# metadata
+__version__ = "0.1.0"
+__author__ = "Milan Staffehl"
+__copyright__ = "(c) Milan Staffehl 2026"
+
 # register Sphinx roles and directives - always required
 register_sphinx_reference_roles()
 register_sphinx_version_notice_directives()
