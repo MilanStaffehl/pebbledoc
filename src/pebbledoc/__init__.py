e90cadbcfc42dc312c443c7265e07498afdb576a
Milan Staffehl <milan-staffehl@gmx.de>
2026-08-11 21:32:28 +0200

    Add useful members of pebbledoc to its API

--- src/pebbledoc/__init__.py
+++ src/pebbledoc/__init__.py
@@ -1,10 +1,19 @@
 from .directives import register_sphinx_version_notice_directives
+from .inspect_runtime import discover_public_members, markdown_documentation
+from .parsing import parse_docstring
 from .roles import register_sphinx_reference_roles
 
 # metadata
 __version__ = "0.1.0"
 __author__ = "Milan Staffehl"
 __copyright__ = "(c) Milan Staffehl 2026"
+
+# API
+__all__ = [
+    "discover_public_members",
+    "parse_docstring",
+    "markdown_documentation",
+]
 
 # register Sphinx roles and directives - always required
 register_sphinx_reference_roles()
