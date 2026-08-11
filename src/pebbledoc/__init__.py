824eda70e0485538f63d2a4d2fa142669e7d9810
Milan Staffehl <52548777+MilanStaffehl@users.noreply.github.com>
2026-08-11 23:29:07 +0200

    Add useful members to pebbledoc's API (#64)
    * Add useful members of pebbledoc to its API
    
    * Add a docstring to the pebbledoc __init__.py

--- src/pebbledoc/__init__.py
+++ src/pebbledoc/__init__.py
@@ -1,10 +1,32 @@
+"""
+pebbledoc offers utilities for creating documentation.
+
+Primarily, ``pebbledoc`` acts as a command line interface (CLI) tool to
+create GitHub-flavored Markdown documentation. See the README of the
+project for details or run ``pebbledoc --help`` to get an overview over
+its functionalities.
+
+However, for convenience, ``pebbledoc`` also exposes some of its more
+generally useful functions for programmatic use in Python scripts and
+libraries.
+"""
+
 from .directives import register_sphinx_version_notice_directives
+from .inspect_runtime import discover_public_members, markdown_documentation
+from .parsing import parse_docstring
 from .roles import register_sphinx_reference_roles
 
 # metadata
 __version__ = "0.1.0"
 __author__ = "Milan Staffehl"
 __copyright__ = "(c) Milan Staffehl 2026"
+
+# API
+__all__ = [
+    "discover_public_members",
+    "parse_docstring",
+    "markdown_documentation",
+]
 
 # register Sphinx roles and directives - always required
 register_sphinx_reference_roles()
